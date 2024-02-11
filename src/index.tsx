7b2f1505436fce2083fd7666fb1bb7594b163ee9
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2024-02-11 09:13:36 +0100

    mv

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { App } from "./components/App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
