2a2501f2d5f9f9e4b8584412edde9e527a158c22
Fran Gonzalez <frgonz@microsoft.com>
2024-03-25 21:00:42 +0100

    Switch to monorepo

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    preset: 'ts-jest',
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-        '^.+\\.(js|jsx)$': 'babel-jest',
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "packages/**/src/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/node_modules/",
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 0,
+      statements: 0,
     },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-    collectCoverage: false,
-    collectCoverageFrom: [
-        "src/**/*.{js,jsx,ts,tsx}",
-        "!<rootDir>/node_modules/"
-    ],
-    coverageThreshold: {
-        global: {
-            "lines": 0,
-            "statements": 0
-        }
-    },
+  },
 };
